d2cd2bb517d3cb2c2cd8f05648d914afba2675ed
Alexandre Roux Tekartik <alex@tekartik.com>
2017-06-01 11:48:18 +0200

    feat: dartium unsafe launch

---
+++ lib/grind/dartium.dart
@@ -0,0 +1,10 @@
+//import 'package:process_run/cmd_run.dart' hide runCmd;
+import 'package:grinder/grinder.dart';
+import 'package:tekartik_build_utils/common_import.dart';
+
+@Task()
+dartium() {
+  runCmd(processCmd("dartium",
+      ['--disable-web-security', '--user-data-dir', '~/.dartium_safe'],
+      environment: {'DART_FLAGS': '--checked'}));
+}

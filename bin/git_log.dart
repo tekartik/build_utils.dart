557709c86c9090a9f5d988161d1969a12edf326f
Alexandre Roux Tekartik <alex@tekartik.com>
2017-10-13 09:01:57 +0200

    feat: add log utils

---
+++ bin/git_log.dart
@@ -0,0 +1,12 @@
+#!/usr/bin/env dart
+import 'dart:async';
+import 'package:tekartik_build_utils/git/git_log.dart';
+
+Future main(List<String> arguments) async {
+  //debugQuickLogging(Level.FINE);
+  //arguments = ["$appleStartupCmd", "/media/ssd/devx/git/github.com/tekartik/tekartik_build_utils.dart/example/genimg/startup_logo.png"];
+  //arguments = ["$appleIconCmd", "/media/ssd/devx/git/github.com/tekartik/tekartik_build_utils.dart/example/genimg/logo_on_white.png"];
+
+  await gitLog();
+  //await genImgConvert(cmd, filePath);
+}
---
+++ lib/git/git_log.dart
@@ -0,0 +1,8 @@
+import 'package:tekartik_build_utils/common_import.dart';
+import 'package:tekartik_sc/git.dart';
+
+gitLog() async {
+  GitPath gitPath = new GitPath();
+  ProcessCmd cmd = gitPath.cmd(["log", '--format=format:%ai | %ae | %s']);
+  await runCmd(cmd);
+}

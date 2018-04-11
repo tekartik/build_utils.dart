8e52e04bb982c4bf664a700e6fe350c4a23a1191
Alexandre Roux Tekartik <alex@tekartik.com>
2018-04-11 10:54:36 +0200

    feat: add dartium helper

---
+++ lib/chrome/dartium_main.dart
@@ -0,0 +1,5 @@
+import 'package:tekartik_build_utils/chrome/chrome.dart';
+
+main() {
+  dartium();
+}
--- lib/grind/dartium.dart
+++ lib/grind/dartium.dart
@@ -1,10 +1,8 @@
 //import 'package:process_run/cmd_run.dart' hide runCmd;
+import '../chrome/chrome.dart' as chrome;
 import 'package:grinder/grinder.dart';
-import 'package:tekartik_build_utils/common_import.dart';
 
 @Task()
-dartium() {
-  runCmd(processCmd("dartium",
-      ['--disable-web-security', '--user-data-dir=~/.dartium_safe'],
-      environment: {'DART_FLAGS': '--checked'}));
+dartium() async {
+  await chrome.dartium();
 }

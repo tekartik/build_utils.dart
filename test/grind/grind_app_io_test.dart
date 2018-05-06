ed8a96abfa454f63f3f2c621e5494674b458c6f6
Alexandre Roux <alex@tekartik.com>
2018-05-06 22:04:42 +0200

    feat: dart2 build support

--- test/grind/grind_app_io_test.dart
+++ test/grind/grind_app_io_test.dart
@@ -15,7 +15,7 @@ main() {
       expect(
           await new File(join("build", "deploy", "web", "index.html")).exists(),
           isTrue);
-    }, timeout: new Timeout.factor(2));
+    }, timeout: new Timeout.factor(5));
 
     test('build_example_browser', () async {
       await runCmd(grindCmd(["example_browser", "build"]), verbose: false);
@@ -24,7 +24,7 @@ main() {
                   join("build", "deploy", "example", "browser", "index.html"))
               .exists(),
           isTrue);
-    }, timeout: new Timeout.factor(2));
+    }, timeout: new Timeout.factor(5));
     test('ping', () async {
       ProcessResult result = await runCmd(grindCmd(["ping"]), verbose: false);
       expect(result.stdout, contains("pong"));

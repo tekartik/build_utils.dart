8826e328a83d885d7dcd444ece5992d870d6422f
Alexandre Roux <alex@tekartik.com>
2018-10-10 15:34:01 +0200

    feat: remove binaries

--- bin/genimg.dart
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env dart
-library tekartik_script.bin.genimg;
-
-import 'package:tekartik_build_utils/bin/genimg.dart' as _;
-
-main(List<String> arguments) => _.main(arguments);
--- bin/git_log.dart
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env dart
-library tekartik_script.bin.git_log;
-
-import 'package:tekartik_build_utils/bin/git_log.dart' as _;
-
-main(List<String> arguments) => _.main(arguments);

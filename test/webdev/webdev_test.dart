644af655215af6c3b993572d840c748f31c48ca3
alex <alex@tekartik.com>
2019-09-06 12:52:25 +0200

    feat: add webdevActivate()

---
+++ test/webdev/webdev_test.dart
@@ -0,0 +1,24 @@
+@TestOn('vm')
+import 'package:dev_test/test.dart';
+import 'package:tekartik_build_utils/grind/grind_app.dart';
+import 'package:tekartik_build_utils/travis/travis.dart';
+import 'package:tekartik_build_utils/webdev/webdev.dart';
+
+void main() {
+  bool _isTravisSupported = isTravisSupportedSync;
+  group('webdev', () {
+    group('activate', () {
+      test('check', () async {
+        await webdevActivate();
+      });
+    });
+    if (_isTravisSupported) {
+      test('version', () async {
+        var result =
+            await runCmd(ProcessCmd(travisExecutableName, ['version']));
+        var version = Version.parse((result.stdout as String).trim());
+        expect(version, greaterThanOrEqualTo(Version(1, 8, 9)));
+      });
+    }
+  });
+}
